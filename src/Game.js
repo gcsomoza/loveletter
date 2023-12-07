803b9c4353a838d0825f3567854c216ed8afea5e
Gene Christian Somoza <somozagenechristian@gmail.com>
2023-12-07 19:35:52 +0800

    Game turn

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,22 @@
 class Game {
     constructor(players, deck) {
+        this.current_player = -1;
         this.players = players;
         this.deck = deck;
 
         for (let i = 0; i < this.players.length; i++) {
+            this.players[i].game = this;
             this.deck.deal(this.players[i]);
         }
+    }
+
+    turn() {
+        this.current_player = (this.current_player + 1) % this.players.length;
+
+        while (this.players[this.current_player].out_of_the_round) {
+            this.current_player = (this.current_player + 1) % this.players.length;
+        }
+
+        this.players[this.current_player].draw(this.deck);
     }
 }
--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,17 @@
 class Player {
     constructor(name) {
+        this.game = null;
         this.name = name;
         this.cards = [];
+        this.out_of_the_round = false;
+    }
+
+    draw(deck) {
+        this.cards.push(deck.cards.shift());
     }
 
     play() {
-        
+
     }
 
     discard() {
